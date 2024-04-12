fe8372c27c5235932521a56fc991981d1db3fc4c
Massimo Ginella <massimoginella12@gmail.com>
2024-04-12 13:24:48 -0700

    Commiting to access other branches

--- src/seahawk/seahawk_deck/dash_widgets/pressure_depth_widget.py
+++ src/seahawk/seahawk_deck/dash_widgets/pressure_depth_widget.py
@@ -11,19 +11,19 @@ class pressure_depth(qtw.QWidget):
 
     def __init__(self, parent:str, style_sheet_file:str, colors: dict):
         super().__init__(parent)
-        self.pressure_data = None
-        self.depth_data = None
+        self.pressure_data = None  # var for pressure data from pressure topic
+        self.depth_data = None  # var for depth data from depth topic
 
-        self.init_ros()
-        self.init_ui()
+        self.init_ros()  # function for setting up subscribers for depth & pressure topics
+        self.init_ui()  # initializes the widget UI
 
     def init_ros(self):
-        self.node = rclpy.create_node('Pressure_widget_node')
+        self.node = rclpy.create_node('Pressure_widget_node')  # Create node that recieves topic data
 
         self.pressure_subscription = self.create_subscription(
-            Pressure,'pressure_topic', self.sub_callback_pressure, 10)
+            Pressure,'pressure_topic', self.sub_callback_pressure, 10)  # Create subscription to pressure topic
 
-        self.depth_subscription = self.create_subscription(
+        self.depth_subscription = self.create_subscription(  # Create subscription to depth topic
             Depth,'depth_topic', self.sub_callback_depth, 10)
 
 
@@ -40,25 +40,23 @@ class pressure_depth(qtw.QWidget):
         inner_layout.addStretch()
 
         self.pressure_title = qtw.QLabel(parent)
-        self.pressure_title.setText(f"Pressure: ")
 
         self.depth_title  = qtw.QLabel(parent)
-        self.depth_title.setText(f"Depth: ")
 
 
     def sub_callback_pressure(self, msg):
-        self.pressure_data = msg.data
+        self.pressure_data = msg.data  # put the topic data in the pressure_data variable
         self.update_pressure_ui()
     
     def sub_callback_depth(self, msg):
-        self.depth_data = msg.data
+        self.depth_data = msg.data  # put the topic data in the depth_data variable
         self.update_depth_ui()
 
     def update_pressure_ui(self):
-        self.pressure_title.setText(f"Pressure: {pressure_data}")
+        self.pressure_title.setText(f"Pressure: {pressure_data}")  # update the pressure value on the widget
 
     def update_depth_ui(self):
-        self.depth_title.setText(f"Depth: {depth_data}")
+        self.depth_title.setText(f"Depth: {depth_data}") # update the depth value on the widget
 
 
 
