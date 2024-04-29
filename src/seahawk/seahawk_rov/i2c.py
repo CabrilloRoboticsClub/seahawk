c25fde84fd6b63b9e4091cb18c2fef3bba6db80c
steph1111 <stephanielh1111@gmail.com>
2024-04-29 02:12:46 -0700

    Made the i2c mega node less mega
    The i2c node was getting a bit too convoluted, this commit moves each indivisual sensor to its own class replicating the mega node code from last year.

--- src/seahawk/seahawk_rov/i2c.py
+++ src/seahawk/seahawk_rov/i2c.py
@@ -26,109 +26,76 @@ cabrillorobotics@gmail.com
 import sys
 import rclpy
 from rclpy.node import Node
-from seahawk_msgs.msg import Bme280
-from seahawk_msgs.msg import PressureSensor
 
-from sensor_msgs.msg import Imu
-import adafruit_bno08x
-from adafruit_bno08x.i2c import BNO08X_I2C
-from adafruit_bme280 import basic as adafruit_bme280
+from seahawk_rov.i2c_sensors.bno085 import BNO085
+from seahawk_rov.i2c_sensors.bme280 import BME280
 
-import ms5837
-from ms5837 import MS5837_02BA(bus=1)
+# from seahawk_msgs.msg import PressureSensor
+# import ms5837
+# from ms5837 import MS5837_02BA(bus=1)
 
 import board
 import busio
 
 class I2C(Node):
     """
-    Class collects data from all sensors on i2c.
+    Class which handles all sensors on the Raspberry Pi I2C bus.
     """
     
     def __init__(self):
         """
-        Initialize 'claws' node.
+        Initialize `i2c` mega node.
         """
         super().__init__("i2c")
         # Grab the i2c interface for us to use
         i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+        self.bno085 = BNO085(self, i2c)  # IMU
+        self.bme280 = BME280(self, i2c)  # Pressure, Temperature, Humidity
         
-        # IMU
-        self.imu_pub = self.create_publisher(Imu, "imu", 10)
-        self.bno085 = BNO08X_I2C(i2c_bus=i2c, address=0x4a)
-        # enable raw data outputs
-        self.bno.enable_feature(adafruit_bno08x.BNO_REPORT_GEOMAGNETIC_ROTATION_VECTOR)
-        self.bno.enable_feature(adafruit_bno08x.BNO_REPORT_GYROSCOPE)
-        self.bno.enable_feature(adafruit_bno08x.BNO_REPORT_LINEAR_ACCELERATION)
-
-        # BME280
-        self.bme_pub = self.create_publisher(Bme280, "bme280", 10)
-        self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus=i2c, address=0x76)
 
         # PRESSURE SENSOR
-        self.pressure_publisher_ = self.create_publisher(PressureSensor, 'pressure_topic', 10)  # create a publisher that publishes messages of type String to pressure_topic
+        # TODO: Migrate all this code to its own class
+        # self.pressure_publisher_ = self.create_publisher(PressureSensor, 'pressure_topic', 10)  # create a publisher that publishes messages of type String to pressure_topic
 
-        sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, i2c_bus=i2c) # Specify model and bus
+        # sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, i2c_bus=i2c) # Specify model and bus
 
-        sensor.init()  # initialize the sensor
+        # sensor.init()  # initialize the sensor
 
-        if not sensor.init():
-            print("SENSOR READ FAILED.")
-            exit(1)
+        # if not sensor.init():
+        #     print("SENSOR READ FAILED.")
+        #     exit(1)
 
-        sensor.read(ms5837.OSR_256)  # Read the sensor and update the pressure and temperature.
+        # sensor.read(ms5837.OSR_256)  # Read the sensor and update the pressure and temperature.
 
-        if not sensor.read():
-            print("SENSOR READ FAILED.")
-            exit(1)
+        # if not sensor.read():
+        #     print("SENSOR READ FAILED.")
+        #     exit(1)
         
-        water_density = ms5837.DENSITY_FRESHWATER  # set value for the density of fresh water
-        g = 9.81  # gravitational acceleration in m/s^2
-        sensor.setFluidDensity(water_density)  # Set fluid density 997 kg/m^3
-        pascal = ms5837.UNITS_Pa
+        # water_density = ms5837.DENSITY_FRESHWATER  # set value for the density of fresh water
+        # g = 9.81  # gravitational acceleration in m/s^2
+        # sensor.setFluidDensity(water_density)  # Set fluid density 997 kg/m^3
+        # pascal = ms5837.UNITS_Pa
 
-        timer_period = 0.5  # space messages out by 0.5 seconds
-        self.timer = self.create_timer(timer_period, self.pressure_callback)  # create a timer for when th
+        self.create_timer(0.5, self.pub_callback)
+    
+    def pub_callback(self):
+        self.bno085.pub_callback()
+        self.bme280.pub_callback()
 
-    def imu_callback(self):
-        msg = Imu()
-        msg.header.frame_id = "bno085"
 
-        msg.orientation.x = self.bno085.geomagnetic_quaternion[1]
-        msg.orientation.y = -self.bno085.geomagnetic_quaternion[0]
-        msg.orientation.z = self.bno085.geomagnetic_quaternion[2]
-        msg.orientation.w = self.bno085.geomagnetic_quaternion[3]
-        msg.angular_velocity.x = self.bno085.gyro[1]
-        msg.angular_velocity.y = -self.bno085.gyro[0]
-        msg.angular_velocity.z = self.bno085.gyro[2]
-        msg.linear_acceleration.x = self.bno085.linear_acceleration[1]
-        msg.linear_acceleration.y = -self.bno085.linear_acceleration[0]
-        msg.linear_acceleration.z = self.bno085.linear_acceleration[2]
+    # def pressure_callback(self):
+    #     pressure_msg = PressureSensor()  # create a obj of type Pressure
+    #     pressure_msg.pressure = sensor.pressure(pascal)
 
-        # Is this the right place for this??
-        try:
-            self.imu_pub.publish(msg)
-        except:
-            self.node.get_logger().info("Warning: IMU failed to publish")
-        
-    def bme_callback(self):
-        msg = Bme280()
-        msg.temperature = self.bme280.temperature
-        msg.humidity = self.bme280.humidity
-        msg.pressure = self.bme280.pressure
-        self.bme_pub.publish(msg)
-
-    def pressure_callback(self):
-        pressure_msg = PressureSensor()  # create a obj of type Pressure
-        pressure_msg.pressure = sensor.pressure(pascal)
-
-        pressure_msg.depth sensor.depth()
-        self.pressure_publisher_.publish(pressure_msg)  # publish depth_msg to depth_topic
+    #     pressure_msg.depth sensor.depth()
+    #     self.pressure_publisher_.publish(pressure_msg)  # publish depth_msg to depth_topic
 
 
 def main(args=None):
     rclpy.init(args=args)
     rclpy.spin(I2C())
+    # NOTE: Consider using MultiThreadedExecutor() and MutuallyExclusiveCallbackGroup()s 
+    # if all sensors reading at the same speed is problematic 
     rclpy.shutdown()
 
 
