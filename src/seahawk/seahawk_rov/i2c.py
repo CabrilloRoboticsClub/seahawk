419cc0104caf9e549b8733ed18de54fc760c9ecd
steph1111 <stephanielh1111@gmail.com>
2024-05-17 23:19:31 +0000

    Commented out pressure sensor because it is not connected yet

--- src/seahawk/seahawk_rov/i2c.py
+++ src/seahawk/seahawk_rov/i2c.py
@@ -52,7 +52,7 @@ class I2C(Node):
         i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
         self.bno085 = BNO085(self, i2c)  # IMU
         self.bme280 = BME280(self, i2c)  # Pressure, Temperature, Humidity
-        self.pressure = Pressure(self, i2c)
+        # self.pressure = Pressure(self, i2c)
 
         self.create_timer(0.5, self.pub_callback)
     
